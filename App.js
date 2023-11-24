42ffb035f232778182291738a80ff6140216cb8a
Salim Ali <u14262208@tuks.co.za>
2023-11-24 17:06:55 +0200

    made the home screen scrollable

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, ScrollView, StatusBar } from "react-native";
 
 
 export default function App() {
@@ -6,6 +6,15 @@ export default function App() {
   return (
 
     <SafeAreaView>
+
+      <ScrollView showVerticalScrollIndicator={false}>
+
+        <StatusBar backgroundColor="#ffffff"
+                   barStyle="dark-content" />
+
+
+      </ScrollView>
+
     </SafeAreaView>
 
   );
