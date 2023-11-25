71e5f62b4b59946601408a6862358a006b505e2a
Salim Ali <u14262208@tuks.co.za>
2023-11-25 08:28:14 +0200

    added another test case for the pocket balance card compoenntand converted default app test js

--- __tests__/unit/component/pocket_balance_card.test.js
+++ __tests__/unit/component/pocket_balance_card.test.js
@@ -9,8 +9,16 @@ describe("Testing functionality of the pocket balance card component separately 
         let value = 123.45;
         render(<PocketBalanceCard amount={value} />);
         let result = screen.getByTestId("pocket-balance-amount");
-        // expect(result.props.children.join("")).toEqual("R" + value.toString());
         expect(result.props.children.join("")).toBe("R" + value.toString());
+
+    });
+
+    test("Input value must be rounded off to 2 decimal places", () => {
+
+        let value = 12345.6789;
+        render(<PocketBalanceCard amount={value} />);
+        let result = screen.getByTestId("pocket-balance-amount");
+        expect(result.props.children[1]).toEqual(value.toFixed(2).toString());
 
     });
 
