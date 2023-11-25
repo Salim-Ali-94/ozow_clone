9d7589e0334bbc601ef28199c450394ff69894b9
Salim Ali <u14262208@tuks.co.za>
2023-11-25 09:32:27 +0200

    can't test truncated text for now

--- __tests__/unit/component/pocket_balance_card.test.js
+++ __tests__/unit/component/pocket_balance_card.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react-native";
+// import "@testing-library/jest-native/extend-expect";
 import PocketBalanceCard from "../../../src/components/PocketBalanceCard";
 
 
@@ -21,5 +22,14 @@ describe("Testing functionality of the pocket balance card component separately 
         expect(result.props.children[1]).toEqual(value.toFixed(2).toString());
 
     });
+
+    // test("Input value must be truncated if length exceeds 1 text line", () => {
+
+    //     let value = 1234567890987654.3210;
+    //     render(<PocketBalanceCard amount={value} />);
+    //     let result = screen.getByTestId("pocket-balance-amount");
+    //     expect(result).toHaveStyle({ overflow: "hidden" });
+
+    // });
 
 });
