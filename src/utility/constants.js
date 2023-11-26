1668465eb367c91121becd11e2ede47173fce95f
Salim Ali <u14262208@tuks.co.za>
2023-11-26 16:17:02 +0200

    added some icons

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,10 +1,10 @@
 
+
 export const primary = "#00e588";
 export const secondary = "#96a7ff";
 export const background = "#f9f9f7";
 export const primary_blend1 = "#01e189";
 export const primary_blend2 = "#00bbaa";
-
 
 export const actions = [{ icon: require("../assets/icons/cash.png"),
                           category: "Get Paid",
