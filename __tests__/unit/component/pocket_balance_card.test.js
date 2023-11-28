d3843ec4578e1d6b683a78e3ca8122f46b0dff51
Salim Ali <u14262208@tuks.co.za>
2023-11-28 09:40:39 +0200

    mocked the navigator inside the pocket balance card cmoponent

--- __tests__/unit/component/pocket_balance_card.test.js
+++ __tests__/unit/component/pocket_balance_card.test.js
@@ -1,12 +1,15 @@
 import { render, screen } from "@testing-library/react-native";
 // import "@testing-library/jest-native/extend-expect";
 import PocketBalanceCard from "../../../src/components/PocketBalanceCard";
+import { useNavigation } from "@react-navigation/native";
 
+
+jest.mock("@react-navigation/native", () => ({ ...jest.requireActual("@react-navigation/native"), useNavigation: jest.fn() }));
 
 describe("Testing functionality of the pocket balance card component separately from the rest of the project", () => {
 
     test("Input value must reflect on the card", () => {
-
+        useNavigation.mockReturnValue({});
         let value = 123.45;
         render(<PocketBalanceCard shadow={true} amount={value} />);
         let result = screen.getByTestId("pocket-balance-amount");
@@ -15,7 +18,7 @@ describe("Testing functionality of the pocket balance card component separately 
     });
 
     test("Input value must be rounded off to 2 decimal places", () => {
-
+        useNavigation.mockReturnValue({});
         let value = 12345.6789;
         render(<PocketBalanceCard shadow={false} amount={value} />);
         let result = screen.getByTestId("pocket-balance-amount");
@@ -24,7 +27,7 @@ describe("Testing functionality of the pocket balance card component separately 
     });
 
     // test("Input value must be truncated if length exceeds 1 text line", () => {
-
+        // useNavigation.mockReturnValue({});
     //     let value = 1234567890987654.3210;
     //     render(<PocketBalanceCard shadow={true} amount={value} />);
     //     let result = screen.getByTestId("pocket-balance-amount");
