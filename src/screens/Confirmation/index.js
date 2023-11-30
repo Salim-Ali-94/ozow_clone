1d9564acbe5320b905f923f202e392b5be6c4fa2
Salim Ali <u14262208@tuks.co.za>
2023-11-30 03:57:13 +0200

    fixed the bug for real now

--- src/screens/Confirmation/index.js
+++ src/screens/Confirmation/index.js
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import LottieView from "lottie-react-native";
 import { screenContext } from "../../providers/screenContext";
@@ -12,21 +12,104 @@ export default function Confirmation() {
     const navigation = useNavigation();
     const { animation, header } = route.params;
     const { setPrevious, setScreen, screen } = useContext(screenContext);
+    const [text, setText] = useState(header);
+    
+
+    // useEffect(() => {
+
+    //     const unsubscribe = navigation.addListener("focus", () => {
+
+    //         setTimeout(() => { setText("Success!"); }, 2000);
+
+    //         // setTimeout(() => { constants.tabBarRef?.current?.setVisible(true);
+    //         //                 setPrevious(screen);
+    //         //                 setScreen("Home");
+    //         //                 navigation.navigate("Home"); }, 2000);
+
+    //     });
+
+    //     // const subscribe = navigation.addListener("focus", () => {
+
+    //     //     setTimeout(() => { constants.tabBarRef?.current?.setVisible(true);
+    //     //                     setPrevious(screen);
+    //     //                     setScreen("Home");
+    //     //                     navigation.navigate("Home"); }, 1000);
+
+    //     // });
+
+    //     return unsubscribe;
+
+    // }, [navigation]);
+
+    // useEffect(() => {
+
+    //     // const unsubscribe = navigation.addListener("focus", () => {
+
+    //     //     setTimeout(() => { setText("Success!"); }, 2000);
+
+    //     //     // setTimeout(() => { constants.tabBarRef?.current?.setVisible(true);
+    //     //     //                 setPrevious(screen);
+    //     //     //                 setScreen("Home");
+    //     //     //                 navigation.navigate("Home"); }, 2000);
+
+    //     // });
+
+    //     const subscribe = navigation.addListener("focus", () => {
+
+    //         setTimeout(() => { constants.tabBarRef?.current?.setVisible(true);
+    //                         setPrevious(screen);
+    //                         setScreen("Home");
+    //                         navigation.navigate("Home"); }, 1000);
+
+    //     });
+
+    //     return subscribe;
+    //     // return () => {
+    //     //     unsubscribe();
+    //     //     subscribe();
+    //     // };
+
+    // }, [text]);
+
+
+    // useEffect(() => {
+    //     const unsubscribe = navigation.addListener("focus", () => {
+    //         // Change the text
+    //         setText("Success!");
+    
+    //         // Navigate after a delay
+    //         setTimeout(() => {
+    //             constants.tabBarRef?.current?.setVisible(true);
+    //             setPrevious(screen);
+    //             setScreen("Home");
+    //             navigation.navigate("Home");
+    //         }, 1000);
+    //     });
+    
+    //     return unsubscribe;
+    // }, [navigation, screen]);
+
 
     useEffect(() => {
-
         const unsubscribe = navigation.addListener("focus", () => {
+            // Delay before changing the text to "Success!"
+            setTimeout(() => {
+                setText("Success!");
+    
+                // Navigate after another delay
+                setTimeout(() => {
+                    constants.tabBarRef?.current?.setVisible(true);
+                    setPrevious(screen);
+                    setScreen("Home");
+                    navigation.navigate("Home");
+                }, 1000);
 
-            setTimeout(() => { constants.tabBarRef?.current?.setVisible(true);
-                            setPrevious(screen);
-                            setScreen("Home");
-                            navigation.navigate("Home"); }, 2000);
+            }, 2000);
 
         });
-
+    
         return unsubscribe;
-
-    }, [navigation]);
+    }, [navigation, screen]);
 
     return (
     
@@ -41,7 +124,8 @@ export default function Confirmation() {
         // lineHeight: 33,
         marginBottom: 30,
         marginTop: 50
-    }}>{header}</Text>
+    // }}>{header}</Text>
+}}>{text}</Text>
  
             <LottieView source={animation} style={{ width: 260, height: 260 }} autoPlay loop />
 
