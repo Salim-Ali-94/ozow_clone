56c94560de2e84f62d432b20287eb5024b27211e
Salim Ali <u14262208@tuks.co.za>
2023-12-03 05:02:52 +0200

    cleaned up the code and added props to the equity card component

--- src/components/EquityCard/index.js
+++ src/components/EquityCard/index.js
@@ -2,97 +2,69 @@ import { View, Pressable, Image, Text } from "react-native";
 import { LineChart } from "react-native-gifted-charts";
 import * as constants from "../../utility/constants";
 import LinearGradient from "react-native-linear-gradient";
-import {SvgUri} from "react-native-svg";
+import { SvgUri } from "react-native-svg";
+import { styles } from "./styles";
 
 
-export default function EquityCard({ data }) {
+export default function EquityCard({ data, company, ticker, price, high, low }) {
 
     return (
 
-        // <Pressable>
-
         <LinearGradient colors={[constants.secondary, constants.primary]} 
-                        // style={{width: "90%", height: 180, borderRadius: 20, backgroundColor: "#000", justifyContent: "center", alignItems: "center", padding: 10 }}
-                        // style={{width: "90%", height: 180, borderRadius: 20, padding: 10 }}
-                        style={{overflow: "hidden", width: "90%", height: 220, borderRadius: 20, padding: 16 }}
+                        style={styles.card}
                         start={{ x: 0, y: 0 }}
                         end={{ x: 1, y: 1 }}>
 
-<Pressable onPress={() => console.log("click")}>
-<View style={{alignItems: "flex-start", flexDirection: "row", justifyContent: "space-between" }}>
-            <View style={{alignItems: "center", justifyContent: "center" }}>
+            <Pressable onPress={() => console.log("click")}>
 
-                <View style={{width: 60, height: 60, borderRadius: 50, backgroundColor: "#fff", justifyContent: "center", alignItems: "center" }}>
+                <View style={styles.detailsSection}>
 
-                    {/* <Image source={{uri: "https://api-ninjas-data.s3.us-west-2.amazonaws.com/logos/lefd12553d6a4f7e57b3ac4f4927181d7a651d0d6.png"}} style={{width:50, height: 50}} /> */}
-                        {/* <Image source={require("../../assets/icons/tsla.png")} style={{width:80, height: 80}} /> */}
-                    {/* <Image source={{uri: "https://api-ninjas-data.s3.us-west-2.amazonaws.com/logos/lfc4548375734de4aa0151bef377e72b1708b5072.png"}} style={{width:50, height: 50}} /> */}
-                    <SvgUri
-                width={40}
-                height={40}
-                fill={"deeppink"}
-                // color={"deeppink"}
-                uri="https://cdn.jsdelivr.net/npm/simple-icons@v10/icons/twitter.svg"
-            />
-            </View>
+                    <View style={styles.descriptionContainer}>
 
-              <Text style={{color: "#fff", fontFamily: "poppins_bold", fontSize: 24, }}>TWTR</Text>
-        
-            </View>
-            
-            <View style={{alignItems: "flex-end", marginBottom: 5 }}>
-<View style={{margin: 0}}>
-                <Text style={{color: "#fff", fontFamily: "poppins_bold", fontSize: 24, lineHeight: 27, padding: 0 }}>$103.45</Text>
+                        <View style={styles.logoBox}>
+
+                            <SvgUri width={40}
+                                    height={40}
+                                    fill={"deeppink"}
+                                    uri={`https://cdn.jsdelivr.net/npm/simple-icons@v10/icons/${company.toLowerCase()}.svg`} />
+
+                        </View>
+
+                        <Text style={styles.indicator}>{ticker.toUpperCase()}</Text>
+
+                    </View>
+                
+                    <View style={styles.priceSection}>
+
+                        <Text style={styles.currentPrice}>${price.toFixed(2)}</Text>
+                        <Text style={styles.cash}>${high.toFixed(2)}</Text>
+                        <Text style={styles.cash}>${low.toFixed(2)}</Text>
+
+                    </View>
+
                 </View>
-                <Text style={{color: "#fff", fontFamily: "poppins_semi_bold", fontSize: 18, padding: 0 }}>$105.63</Text>
-                <Text style={{color: "#fff", fontFamily: "poppins_semi_bold", fontSize: 18, padding: 0 }}>$98.42</Text>
 
-            </View>
-            </View>
             </Pressable>
-            {/* </View> */}
+
             <LineChart curved
-                    areaChart
-                    showYAxisIndices={false}
-                    hideYAxisText
-                    yAxisThickness={0}
-                    xAxisThickness={0}
-                    hideDataPoints
-                    hideAxesAndRules
-                    data={data}
-                    // height={100}
-                    height={80}
-                    // height={65}
-                    // height={"100%"}
-                    // width={360}
-                    thickness1={1.5}
-                    // thickness1={1.25}
-                    // color="rgb(46, 217, 255)"
-                    // color={constants.cement}
-                    // color={"#000"}
-                    color={"deeppink"}
-                    // color={"#fff"}
-                    // startFillColor="rgb(46, 217, 255)"
-                    // startFillColor="rgb(255, 255, 255)"
-                    // startFillColor="rgb(0, 0, 0)"
-                    // startFillColor="deeppink"
-                    //    startFillColor={constants.primary_blend1}
-                    //    startFillColor={constants.primary}
+                       areaChart
+                       showYAxisIndices={false}
+                       hideYAxisText
+                    //    yAxisThickness={0}
+                    //    xAxisThickness={0}
+                       hideDataPoints
+                       hideAxesAndRules
+                       data={data}
+                       height={80}
+                       thickness1={1.5}
+                       color={"deeppink"}
                        startFillColor={constants.secondary}
-                    // start={0}
-                    // end={0.1}
-                    // edgePosition={}
-                    startOpacity={0.5}
-                    // endFillColor="rgb(203, 241, 250)"
-                    // endFillColor="deeppink"
-                    //    endFillColor={constants.primary_blend2}
-                    //    endFillColor={constants.secondary}
+                       startOpacity={0.5}
                        endFillColor={constants.primary}
-                    endOpacity={0} />
-            {/* </Pressable> */}
+                       endOpacity={0} />
 
         </LinearGradient>
-        // </Pressable>
+
     );
 
 }
--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -109,7 +109,12 @@ export default function Home() {
           </View>
 
           <View style={{alignItems: "center"}}>
-            <EquityCard data={[{value: 15}, {value: 30}, {value: 26}, {value: 40},{value: 20},{value: 18},{value: 40},{value: 36},{value: 60},{value: 54},{value: 85},{value: 10},{value: 8},{value: 58},{value: 56},{value: 78},{value: 74},{value: -20},{value: 98}]} />
+            <EquityCard data={[{value: 15}, {value: 30}, {value: 26}, {value: 40},{value: 20},{value: 18},{value: 40},{value: 36},{value: 60},{value: 54},{value: 85},{value: 10},{value: 8},{value: 58},{value: 56},{value: 78},{value: 74},{value: -20},{value: 98}]}
+                        company={"tEslA"}
+                        ticker={"tsla"}
+                        price={103.45}
+                        high={105.63}
+                        low={98.42} />
           </View>
           
           <View style={styles.section}>
